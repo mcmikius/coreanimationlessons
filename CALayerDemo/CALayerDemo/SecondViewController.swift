cd65dde72219efae86c028478fb3cd316a1fdbd4
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-13 12:14:53 +0200

    Add button and image properties

--- CALayerDemo/CALayerDemo/SecondViewController.swift
+++ CALayerDemo/CALayerDemo/SecondViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = imageView.frame.size.height / 2
+            imageView.layer.masksToBounds = true
+            let borderColor = UIColor.white
+            imageView.layer.borderColor = borderColor.cgColor
+            imageView.layer.borderWidth = 10
+            imageView.layer.shadowOffset = CGSize(width: 0, height: 10)
+            imageView.layer.shadowOpacity = 1
+            imageView.layer.shadowRadius = 10
+    
+        }
+        
+        
+    }
+    @IBOutlet weak var button: UIButton! {
+        didSet {
+            button.layer.cornerRadius = button.frame.size.height / 2
+            button.layer.shadowOffset = CGSize(width: 0, height: 10)
+            button.layer.shadowOpacity = 1
+            button.layer.shadowRadius = 10
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- CALayerDemo/CALayerDemo/ViewController.swift
+++ CALayerDemo/CALayerDemo/ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton! {
+        didSet {
+            button.layer.shadowOffset = CGSize(width: 0, height: 5)
+            button.layer.shadowOpacity = 0.5
+            button.layer.shadowRadius = 5
+            button.layer.cornerRadius = 15
+            button.layer.masksToBounds = true
+        }
+    }
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = imageView.frame.size.height / 2
+            imageView.layer.masksToBounds = true
+            let borderColor = UIColor.white
+            imageView.layer.borderColor = borderColor.cgColor
+            imageView.layer.borderWidth = 10
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
